15603d1693fbf36dbc27e67d0daf9e5d7376405c
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-22 16:46:02 -0300

    feat: implementando o próprio component modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
+
+Modal.setAppElement("#root");
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
@@ -20,12 +23,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transaction</h2>
-      </Modal>
+      />
       <GlobalStyle />
     </>
   );
