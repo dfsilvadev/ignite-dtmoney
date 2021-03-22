c3a97686b145d2ffc0259da035e2e09c20b59b0c
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-22 11:01:43 -0300

    feat: importando o component Header e usando o fragmento do react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>DTMONEY</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
