29c2621a4292fb769f3e4883a11f5f9d1e22612d
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-22 11:01:59 -0300

    feat: implementando o component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import logoImg from "../../assets/logo.svg";
+
+import { Container, Content } from "./styles";
+
+export function Header() {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" title="logo dt money" />
+        <button type="button">Nova Transação</button>
+      </Content>
+    </Container>
+  );
+}
