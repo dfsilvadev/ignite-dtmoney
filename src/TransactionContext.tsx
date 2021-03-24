f7a616b6350baa07f1b614c971c630432d4f793f
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-24 13:46:34 -0300

    feat: criando o context e trazendo a criação de transaction

---
+++ src/TransactionContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "./services/api";
+
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
+type TransactionInput = Omit<Transaction, "id" | "createdAt">;
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+);
+
+export function TransactionProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api
+      .get("transactions")
+      .then((reponse) => setTransactions(reponse.data.transactions));
+  }, []);
+
+  function createTransaction(transaction: TransactionInput) {
+    api.post("/transactions", transaction);
+  }
+
+  return (
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
