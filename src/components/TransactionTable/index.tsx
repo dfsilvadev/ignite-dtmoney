f64e7316a7c60349d575188b78aa79dd9b7d86cf
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-24 11:45:34 -0300

    feat: formatando os valores date e amount

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -11,11 +11,6 @@ interface Transaction {
   createdAt: string;
 }
 
-// toLocaleString("pt-BR", {
-//   style: "currency",
-//   currency: "BRL",
-// })
-
 export function TransactionTable() {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
@@ -39,9 +34,18 @@ export function TransactionTable() {
           {transactions.map((transaction) => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>{transaction.amount}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.amount)}
+              </td>
               <td>{transaction.category}</td>
-              <td>{transaction.createdAt}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
