f11272804eb36fdd1b887bf7c9dde658e80a807f
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-24 11:06:28 -0300

    feat: listando os dados da API no component transactiontable

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,10 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
+// toLocaleString("pt-BR", {
+//   style: "currency",
+//   currency: "BRL",
+// })
+
 export function TransactionTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
   useEffect(() => {
-    api.get("transactions").then((reponse) => console.log(reponse.data));
+    api
+      .get("transactions")
+      .then((reponse) => setTransactions(reponse.data.transactions));
   });
   return (
     <Container>
@@ -18,30 +36,14 @@ export function TransactionTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de site</td>
-            <td className="deposit">R$12.000,00</td>
-            <td>Venda</td>
-            <td>22/03/2021</td>
-          </tr>
-          <tr>
-            <td>Hamburguer</td>
-            <td className="withdraw">- R$59,00</td>
-            <td>Alimentação</td>
-            <td>13/03/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel do apartamento</td>
-            <td className="withdraw">- R$1.200,00</td>
-            <td>Casa</td>
-            <td>10/03/2021</td>
-          </tr>
-          <tr>
-            <td>Computador</td>
-            <td className="deposit">R$5.400,00</td>
-            <td>Venda</td>
-            <td>05/03/2021</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,28 @@ createServer({
   models: {
     transaction: Model,
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelancer de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1200,
+          createdAt: new Date("2021-02-14 11:00:00"),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = "api";
     this.get("/transactions", () => {
